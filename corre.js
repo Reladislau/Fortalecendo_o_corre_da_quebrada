efbdc927fee9e86257e5f88f36451b575ed3a44f
Renan Ladislau <renanladislau9@gmail.com>
2023-09-29 00:43:06 -0300

    Continuação 2.0

--- corre.js
+++ corre.js
@@ -8,3 +8,12 @@ function menuShow() {
         document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
     }
 }
+
+document.getElementById('next').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').appendChild(lists[0]);
+}
+document.getElementById('prev').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').prepend(lists[lists.length - 1]);
+}
