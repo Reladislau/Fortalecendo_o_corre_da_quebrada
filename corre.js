536e27f0a2f23cc1ab49ab058cf26521b4fe3262
Renan Ladislau <renanladislau9@gmail.com>
2023-09-29 01:45:50 -0300

    Atualização 5.0

--- corre.js
+++ corre.js
@@ -9,11 +9,3 @@ function menuShow() {
     }
 }
 
-document.getElementById('next').onclick = function(){
-    let lists = document.querySelectorAll('.item');
-    document.getElementById('slide').appendChild(lists[0]);
-}
-document.getElementById('prev').onclick = function(){
-    let lists = document.querySelectorAll('.item');
-    document.getElementById('slide').prepend(lists[lists.length - 1]);
-}
